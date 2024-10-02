5eaa7fd2d91a36c02bbe84de01d49636e0b6d5ed
carlosgonzalezcomillas <202206299@alu.comillas.edu>
2024-10-02 13:16:41 +0200

    First commit - Uploading Practica1

---
+++ Practica1/Police.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica_1
+{
+    internal class Police : Vehicle
+    {
+        public bool IsPatrolling { get; private set; }
+        private Radar? radar;
+
+        public Police(string plate) : base("Police", plate, 0)
+        {
+            IsPatrolling = false;
+            radar = null; 
+        }
+
+        public override void StartWork()
+        {
+            if (!IsPatrolling)
+            {
+                IsPatrolling = true;
+                radar = new Radar(); 
+                Console.WriteLine($"Police car with plate {Plate}: started patrolling.");
+            }
+            else
+            {
+                Console.WriteLine($"Police car with plate {Plate}: is already patrolling.");
+            }
+        }
+
+        public override void FinishWork()
+        {
+            if (IsPatrolling)
+            {
+                IsPatrolling = false;
+                Console.WriteLine($"Police car with plate {Plate}: stopped patrolling.");
+            }
+            else
+            {
+                Console.WriteLine($"Police car with plate {Plate}: was not patrolling.");
+            }
+        }
+
+        public void UseRadar(Cab cab) 
+        {
+            if (IsPatrolling && radar != null)
+            {
+                Console.WriteLine($"Police car with plate {Plate}: Triggered radar: {radar.MeasureSpeed(cab)}");
+            }
+            else
+            {
+                Console.WriteLine($"Police car with plate {Plate}: has no active radar.");
+            }
+        }
+
+        public void ReportRadarData()
+        {
+            if (radar != null)
+            {
+                Console.WriteLine($"Police car with plate {Plate}: Report radar history:");
+                radar.ShowSpeedHistory(); 
+            }
+            else
+            {
+                Console.WriteLine("There is no radar history because police car with plate {Plate} did not active the radar o was not patrolling.");
+            }
+        }
+    }
+}
---
+++ Practica1/Vehicle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica_1
+{
+    // Base abstract class Vehicle -> Cab or Police
+    public abstract class Vehicle : IMessageWriter
+    {
+        public string Type { get; protected set; }
+        public string Plate { get; protected set; }
+        public int Speed { get; protected set; }
+
+        public Vehicle(string type, string plate, int speed)
+        {
+            Type = type;
+            Plate = plate;
+            Speed = speed;
+        }
+
+        public abstract void StartWork();
+        public abstract void FinishWork();
+
+        public virtual string WriteMessage(string customMessage)
+        {
+            return $"{Type} with plate {Plate}: {customMessage}";
+        }
+
+        public override string ToString()
+        {
+            return WriteMessage("Created");
+        }
+    }
+}
